6ce2eb74bd7160f6528321dacdaf29440cf619b2
lubna-bakkar <lubnabakkar93@yahoo.com>
2022-12-08 13:04:05 +0300

    practice

---
+++ app.js
@@ -0,0 +1,20 @@
+let University=[
+
+information={
+
+    "collegName":"Hijjawi for engineering technology",
+    "departments":[Biomedical,computer,power,civil],
+    "grade":95,
+    "projects":{
+        "primery":1,
+        "final":"DNA"
+
+    }
+},
+location={
+    "country":"Jordan",
+    "city":"Irbid"
+
+}
+
+]
